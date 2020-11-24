8936dedcae5f91924cd8d3058f405cfc6fd9ad3a
Joseph Mutua <Mutuaj793@gmail.com>
2020-11-24 16:29:47 +0300

    Wirinng up the provider

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import { combineReducers } from "redux";
+
 const songsReducer = () => {
   return [
     { title: "As Long As You Love Me", duration: "3:58" },
@@ -13,3 +15,8 @@ const selectedSongReducer = (selectedSong = null, action) => {
   }
   return selectedSong;
 };
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer,
+});
